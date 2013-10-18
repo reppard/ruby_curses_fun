b5416e651657a110e9499cf5c00b8b5aaafaa0e6
reppard <reppard@hotmail.com>
2013-10-17 22:32:54 -0400

    Added message object and a bunch of config stuff

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -1,29 +1,67 @@
 class MessageHandler
   attr_reader :target
   def initialize target
-    @config = YAML.load_file("messages.yaml")
     @target = target
   end
 
   def process(line)
-    extracted = line.chomp.split(' ')
-    code = extracted[1]
-    @config[code] ? eval("display_#{@config[code]['type']}(extracted,code)")
-                  : display_default(extracted,code)
+    msg = Message.new line
+    print_parsed(msg)
     @target.refresh
   end
 
-  def display_default message_array,code
-    (message_array.size >= 3) ? @target.addstr(code+message_array[3..-1].join(' ')+"\n")
-                              : @target.addstr(code+message_array.join(' ')+"\n")
+  def print_parsed msg
+    @target.addstr(msg.format)
   end
 
-  def display_NOTICE message_array,code
-    @target.addstr(code+message_array[3..-1].join(' ')+"\n")
+  def print_unparsed message_array,code
+    @target.addstr(code+message_array.join(' ')+"\n")
+  end
+end
+
+class Message
+  attr_reader :extracted, :format
+  CONFIG = YAML.load_file("messages.yaml")
+
+  def initialize line
+    @extracted = line.split(' ')
+    @from   = @extracted[0]
+    @code   = @extracted[1]
+    @to     = @extracted[2]
+    @format = get_format
   end
 
-  def display_RPL_MOTD message_array,code
-    @target.addstr(code+message_array[3..-1].join(' ')+"\n")
+  def get_format
+    if has_format?
+      format = CONFIG[@code]['format']
+      format.each do |entry|
+        if entry.class == String
+          range = entry.split('..').map{|d| d.to_i}
+          format.delete(entry)
+          format << (range[0]..range[1])
+        end
+      end
+      build_string(format)
+    else
+      default_format
+    end
   end
 
+  def build_string format
+    has_header? ? CONFIG[@code]['header']+format.collect{|index| @extracted[index]}.join(' ')+"\n\n"
+                : format.collect{|index| @extracted[index]}.join(' ')+"\n"
+  end
+
+  def default_format
+    self.extracted.to_s+"\n"
+    #self.extracted.join(' ')+"\n"
+  end
+
+  def has_format?
+    CONFIG[@code] && CONFIG[@code]['format']
+  end
+
+  def has_header?
+    CONFIG[@code] && CONFIG[@code]['header'] 
+  end
 end
