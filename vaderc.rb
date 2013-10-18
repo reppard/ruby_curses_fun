a0b743bf99c935c77990a2d13d494714885418ae
reppard <reppard@hotmail.com>
2013-10-18 09:19:44 -0400

    Added CLI usage message

--- vaderc.rb
+++ vaderc.rb
@@ -3,9 +3,11 @@ Dir["lib/*.rb"].each{|file| require_relative file}
 
 class VadeRC
   def self.run set_config
-      config = YAML.load_file("config.yaml")
-      server = config[set_config]['server']
-      port = config[set_config]['port']
+      @config = YAML.load_file("config.yaml")
+      blow_up if set_config.nil? || !@config.has_key?(set_config)
+
+      server = @config[set_config]['server']
+      port = @config[set_config]['port']
 
       @mainwindow = Window.new
       @socket_reader = SocketReader.new(server,port,@mainwindow)
@@ -13,6 +15,16 @@ class VadeRC
 
       @socket_reader.read_from_server.join
       @socket_writer.input_stream.join
+  end
+
+  def self.blow_up
+    puts "usage: ruby vaderc.rb <config-name>"
+    puts "\nThe current available options are:"
+    @config.each_key do |config|
+      puts "    #{config}"
+    end
+    puts "\nTo add a server configuration, add it to 'config.yaml'."
+    exit
   end
 end
 
