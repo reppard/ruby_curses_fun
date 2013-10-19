47a5f51f1471faf36211ddbcfb63e0f4011364a3
reppard <reppard@hotmail.com>
2013-10-18 21:04:55 -0400

    Extracted command class

--- lib/socket_writer.rb
+++ lib/socket_writer.rb
@@ -1,26 +1,3 @@
-class Command
-  attr_accessor :line, :socket, :payload
-  CONFIG = YAML.load_file("commands.yaml")
-  def initialize line, socket
-    @line = line.chomp
-    @socket = socket
-    @payload = is_command? ? process_command : @line
-  end
-
-  def is_command?
-    @line[0] == "/" && CONFIG.has_key?(stripped_cmd)
-  end
-
-  def process_command
-    cmd_alias, *body = @line.split(' ')
-    "#{CONFIG[stripped_cmd]['command']} #{body.join(' ')}"
-  end
-
-  def stripped_cmd
-    @line.split(' ').first.gsub(/^\//,'').upcase
-  end
-end
-
 class SocketWriter
   attr_reader :input_stream
 
